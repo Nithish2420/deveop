1fce12a9643c94aa2a20c6a3229bec9cf5ffde5c
Nithish <nithishv2420@gmail.com>
2026-07-15 14:31:35 +0530

    addition subtraction

---
+++ addsub.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int addition = a + b;
+    int subtraction = a - b;
+
+    cout << "Addition = " << addition << endl;
+    cout << "Subtraction = " << subtraction << endl;
+
+    return 0;
+}
